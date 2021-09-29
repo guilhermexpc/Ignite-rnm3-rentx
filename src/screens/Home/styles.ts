14a8f1047be6e2dba6e8af145ebb822017b00dcd
Guilherme Campos <guilhermee1219@gmail.com>
2021-09-28 21:58:01 -0300

    Criacao da Home

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,11 @@
-import styled from 'styled-components/natives;
+import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+
 `;
